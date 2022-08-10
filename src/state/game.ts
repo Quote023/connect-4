0b6a480ff7aa47049fd12dd7109887cfb7ac98ae
quote023 <acs2@ecomp.poli.br>
2022-08-10 15:32:38 -0300

    comments

--- src/state/game.ts
+++ src/state/game.ts
@@ -4,6 +4,13 @@ import { Board, GameHistory, GameOverState, Player, PlayerData } from "types";
 import { localStorageEffect } from "./effects";
 
 
+/** 
+ * @author Alex 
+ * On a real production setup 
+ * the extra information about the match, like the play sequence and the board state could be stored on a cdn.
+ * it's not something that needs to be easily available and can get big real quick depending on the number of players
+ * (if i want to see the board of a previous game i can just go to <cdn>/history/<matchid>.json and get the desired info)
+*/
 export const gameHistoryState = atom<GameHistory>({
   key: "gameHistoryState",
   default: [],
